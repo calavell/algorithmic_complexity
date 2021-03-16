0d55523d906695cca175ffbcf102d05b0e6004d3
calavell <calavell@tcd.ie>
2021-03-16 12:25:38 +0000

    preliminary reverse algorithm completed

--- lib/reversing_array.rb
+++ lib/reversing_array.rb
@@ -1,3 +1,7 @@
 def reverse(array)
-    []
+    reversed_array = []
+    array.each do |element|
+        reversed_array.unshift(element)
+    end
+    reversed_array
 end
--- spec/reversing_array_spec.rb
+++ spec/reversing_array_spec.rb
@@ -4,4 +4,12 @@ describe '#reverse' do
     it 'returns empty array when passed an empty array' do
         expect(reverse([])).to eq []
     end
+
+    it 'returns itself when given an array of length 1' do
+        expect(reverse([1])).to eq [1]
+    end
+
+    it 'returns the reverse of an array with multiple elements' do
+        expect(reverse([1, 2, 3])). to eq [3, 2, 1]
+    end
 end
